5746fc0d00cd0296802ebb2ab3ff21da9adf9bc2
Deverver <jackRMrasmussen@gmail.com>
2026-04-03 20:14:46 +0200

    Added "framework" for animation

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,37 +7,45 @@ public class PlayerController : MonoBehaviour
     [field: SerializeField] private float Speed { get; set; } = 2f;
 
     private Rigidbody2D rb;
+    private Animator animator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = new Vector2();
+        Vector2 direction = new Vector2();
 
         if (Input.GetKey(KeyCode.W))
         {
-            move.y += Speed;
+            direction.y = 1;
+            animator.SetInteger("Direction", 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            move.y -= Speed;
+            direction.y = -1;
+            animator.SetInteger("Direction", 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            move.x += Speed;
+            direction.x = 1;
+            animator.SetInteger("Direction", 2);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            move.x -= Speed;
+            direction.x = -1;
+            animator.SetInteger("Direction", 3);
         }
+        
+        direction.Normalize();
 
-
-        rb.MovePosition(rb.position + move * Time.deltaTime);
+        animator.SetBool("IsMoving", direction.magnitude > 0);
+        rb.linearVelocity = Speed * direction;
     }
 }
